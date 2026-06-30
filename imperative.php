51a44c0f8c24c6c7da6ba7b0ffb4012034ea830d
Rougui SY <rougui080601@gmail.com>
2026-06-30 21:54:20 +0000

    initialisation du tableau

---
+++ imperative.php
@@ -0,0 +1,17 @@
+<?php
+    $categories = [
+        [
+            "nom"=>"categorie1",
+            "code"=>"C01",
+            "produits"=>[
+                ["nom"=>"pomme","reference"=>"C123","qte"=>25,"prix"=>100],
+                ["nom"=>"fraise","reference"=>"C124","qte"=>50,"prix"=>100]
+            ]
+        ],
+        [
+            "nom"=>"categorie2",
+            "code"=>"C02",
+            "produits"=>[]
+        ]
+    ];
+
