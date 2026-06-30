949a2dde5242e29423d198f9f33236ce8586f1e4
Rougui SY <rougui080601@gmail.com>
2026-06-30 22:59:02 +0000

    affichage des categories qui n'ont pas de produit

--- imperative.php
+++ imperative.php
@@ -1,4 +1,5 @@
 <?php
+    // question 1
     $categories = [
         [
             "nom"=>"categorie1",
@@ -15,3 +16,10 @@
         ]
     ];
 
+    // question 2
+    foreach ($categories as  $categorie) {
+        if (count($categorie["produits"])==0) {
+            echo"nom :".$categorie["nom"]."\n";
+            echo" code :".$categorie["code"];
+        }
+    }
